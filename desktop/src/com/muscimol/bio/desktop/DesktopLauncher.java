446514b077e9444f0e74acbf3048fd684280d817
acidmors <acid.carpet.studio@gmail.com>
2020-09-11 02:17:57 +0300

    first commit

---
+++ desktop/src/com/muscimol/bio/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.muscimol.bio.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.muscimol.bio.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
